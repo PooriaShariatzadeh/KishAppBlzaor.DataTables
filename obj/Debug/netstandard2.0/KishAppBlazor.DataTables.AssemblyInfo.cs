b3f1bce1f8dc0a13c999d84dd9c1b0cadc029833
Pooria Shariatzadeh <pooria.shariatzadeh@gmail.com>
2020-09-23 11:17:09 +0330

    release

--- obj/Debug/netstandard2.0/KishAppBlazor.DataTables.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/KishAppBlazor.DataTables.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("KishAppBlazor.DataTables")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Pooria Shariatzadeh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("DataTables.Net Blazor Component")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KishAppBlazor.DataTables")]
